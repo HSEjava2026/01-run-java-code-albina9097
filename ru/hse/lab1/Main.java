70a2cbd2d0b9a391eb8a678173ee499a62fb7058
albina9097 <albinadanya2017@gmail.com>
2026-02-04 12:08:11 +0300

    2 task

---
+++ ru/hse/lab1/Main.java
@@ -0,0 +1,7 @@
+import second.Person;
+public class Main{
+    public static void main(String[] args) {
+        Person person = new Person("Иван", 20);
+        System.out.println("Имя:");
+    }
+}
